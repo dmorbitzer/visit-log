4ffcf86a85fd642a2e93285148d5be1a325fb542
Dominik Morbitzer <do.morbitzer@web.de>
2026-05-07 21:50:33 +0200

    [feat] #6: add table tracking_days

---
+++ database/migrations/2026_05_07_194443_create_tracking_days_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tracking_days', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('event_id');
+            $table->date('date');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+
+            $table->unique(['event_id', 'date']);
+            $table->index('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tracking_days');
+    }
+};
